bd2ef18760907ca0b1fb59d8f3da1c6f557fcf46
Benjamin Badger <blbadger10@gmail.com>
2025-06-12 16:38:17 -0400

    one-layer newton

--- mixer_lm/linear_mixer_solver.py
+++ mixer_lm/linear_mixer_solver.py
@@ -1,15 +1,16 @@
 import torch
 from einops import rearrange
 import torch.nn as nn
-from torch.nn.parallel import DistributedDataParallel as DDP
-import torch.distributed as dist
-from torch.utils.data import Dataset, DataLoader
 import transformers
 from transformers import AutoTokenizer, PreTrainedTokenizerFast
 from datasets import load_dataset
 from safetensors.torch import load_model
 import warnings
 from tqdm import tqdm
+import time
+warnings.simplefilter(action='ignore', category=FutureWarning) # for FSDP shard saving
+warnings.simplefilter(action='ignore', category=UserWarning)
+
 
 class LinearBlock(nn.Module):
 
@@ -49,7 +50,7 @@ class LinearMixer(nn.Module):
             for i in range(depth)]
             ).to(device)
         self.lm_head = nn.Linear(dim, n_vocab, bias=False)
-        self.cel = nn.CrossEntropyLoss()
+        self.cel = nn.CrossEntropyLoss(reduction='none')
 
     def forward(self, input_ids, labels=None):
         x = input_ids
@@ -68,60 +69,6 @@ class LinearMixer(nn.Module):
             return loss, output
         else:
             return x
-
-def train_solver(model):
-    gradient = model.grad()
-    # equation: 0 = gradient
-    return minimal_params
-
-
-def newton_method(model, train_data, gradient):
-    pass
-
-def obtain_gradients(model):
-    gpu_count = torch.cuda.device_count()
-    dist.init_process_group("nccl")
-    rank = dist.get_rank()
-    dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
-
-    # create model and move it to GPU with id rank
-    device_id = rank % torch.cuda.device_count()
-    model = model.to(device_id)
-    ddp_model = DDP(model, device_ids=[device_id])
-
-    optimizer = Adam(ddp_model.parameters(), lr=1e-4)
-    ddp_model.train()
-
-    for epoch in tqdm(range(epochs)):
-        start_time = time.time()
-        total_loss = 0
-        total_mse_loss = 0
-        accumulation_loss = torch.tensor(0.).to(device_id)
-        for step, batch in enumerate(dataloader):
-            if len(batch) < batch_size:
-                break 
-            
-            with torch.autocast(device_type=device, dtype=torch.float16, enabled=True):
-                batch = batch.to(device_id) # discard class labels
-                output = ddp_model(batch) 
-                loss = loss_fn(output, batch)
-
-                loss_size = loss.shape
-                loss = torch.sum(loss)
-                total_loss += loss.item()
-            scaler.scale(loss).backward()
-            if (step + 1) % gradient_accumulations == 0:
-                scaler.step(optimizer)
-                scaler.update()
-                optimizer.zero_grad()
-
-        if rank == 0:
-            checkpoint_path = f'/home/bbadger/Desktop/churches_unetwidedeep/epoch_{epoch}'
-            if epoch % 100 == 0: torch.save(ddp_model.state_dict(), checkpoint_path)
-            tqdm.write(f"Epoch {epoch} completed in {time.time() - start_time} seconds")
-            tqdm.write(f"Average Loss: {round(total_loss / step, 5)}")
-            tqdm.write(f"Loss shape: {loss_size}")
-        dist.barrier()
 
 tokenizer = PreTrainedTokenizerFast(tokenizer_file="/home/bbadger/Desktop/tiny_token_16k/tokenizer.json")
 #tokenizer = AutoTokenizer.from_pretrained("/home/bbadger/Desktop/tokenizer_tinystories_16k")
@@ -151,7 +98,7 @@ if __name__ == '__main__':
         return output
 
 
-    def batch_tokenize_input(train_text, test_text, length=2000000, batch_size=1024):
+    def batch_tokenize_input(train_text, test_text, length=20000, batch_size=1024):
         train_data, test_data = [], []
         max_length = 128
 
@@ -185,4 +132,55 @@ if __name__ == '__main__':
     train_data, test_data = batch_tokenize_input(train_text, valid_text)
     train_data, test_data = debatch_input(train_data), debatch_input(test_data)
 
-    train_autoencoder(model, dataset='landscapes')
+    model.train()
+    #trainer.train() 
+    print (model.mixerblocks[0])
+    def train_solver(model, train_data):
+        train_batch = torch.stack(train_data[0:128], dim=0)
+        print (train_batch.shape)
+        train_batch = train_batch.to('cuda') 
+        loss, output = model(train_batch, labels=train_batch) 
+        print (f"Starting loss: {loss.item()}")
+        loss.backward()
+        start_time = time.time()    
+        minimal_wte = torch.pinverse(model.wte.weight.grad) @ torch.zeros(model.wte.weight.shape).to('cuda')
+        print (f'minimal wte found in {time.time() - start_time} seconds')
+        start_time = time.time()
+        minimal_conv = torch.pinverse(model.mixerblocks[0].conv.weight.grad) @ torch.zeros(model.mixerblocks[0].conv.weight.shape).to('cuda')
+        print (f'minimal conv found ion {time.time() - start_time} seconds')
+        start_time = time.time()
+        minimal_lm = torch.pinverse(model.lm_head.weight.grad) @ torch.zeros(model.lm_head.weight.shape).to('cuda')
+        print (f'minimal lm found in {time.time() - start_time} seconds')
+        return minimal_params
+
+    def normal_solve(model, train_data):
+        train_batch = torch.stack(train_data[0:128], dim=0)
+        train_batch = train_batch.to('cuda') 
+        loss, output = model(train_batch, labels=train_batch) 
+        print (f"Starting loss: {loss.item()}")
+        # model.lm_head.weight = torch.inverse(model.lm_head.activations.T @ model.lm_head.activations) @ model.lm_head.activations.T
+        # model.mixerblocks[0].conv.weight = torch.inverse() @ model.conv_activations.T
+        train_batch = torch.nn.functional.one_hot(train_batch.squeeze(1), num_classes = len(tokenizer))
+        print (train_batch.shape)
+        model.wte.weight = torch.inverse(train_batch.T @ train_batch) @ train_batch.T @ actual_output
+        loss, output = model(train_batch, labels=train_batch) 
+        print (f"Ending loss: {loss.item()}")
+        return minimal_params
+
+    def newton_iteration(model, train_data):
+        train_batch = torch.stack(train_data[0:128], dim=0)
+        train_batch = train_batch.to('cuda') 
+        loss, output = model(train_batch, labels=train_batch) 
+        print (f"Starting loss: {loss.item()}")
+        torch.sum(loss).backward() # gradients propegated to params
+        model.lm_head.weight = model.lm_head.weight + loss / (parameter.grad)
+        loss, output = model(train_batch, labels=train_batch) 
+        print (f"Ending loss: {loss.item()}")
+        return 
+
+    newton_iteration(model, train_data)
+
+
+
+
+
